1d4dd0995fbefebdb447e75c6def075cc45111c8
J0WI <J0WI@users.noreply.github.com>
2018-06-05 03:21:33 +0200

    Add update.sh and templates

--- update.sh
+++ update.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+declare -A cmd=(
+	[apache]='apache2-foreground'
+	[fpm]='php-fpm'
+	[fpm-alpine]='php-fpm'
+)
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[fpm-alpine]='alpine'
+)
+
 for variant in apache fpm fpm-alpine; do
 	dir="$variant"
+	template="Dockerfile-${base[$variant]}.template"
 	mkdir -p "$dir"
 	cp -a "docker-entrypoint.sh" "$dir/docker-entrypoint.sh"
+	cp $template "$variant/Dockerfile"
+	sed -ri -e '
+		s/%%VARIANT%%/'"$variant"'/;
+		s/%%CMD%%/'"${cmd[$variant]}"'/;
+	' "$dir/Dockerfile"
 	travisEnv+='\n  - VARIANT='"$dir"
 done
 
